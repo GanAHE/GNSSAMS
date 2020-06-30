b1c1e165cd8a2a19a6bc761ab3b639d4fe2fe565
dinggan <dinggan@whu.edu.cn>
2020-06-30 10:13:13 +0800

    <add>新增启动页功能。让等待不再枯燥

--- EMACS.py
+++ EMACS.py
@@ -8,16 +8,26 @@ comment: 主函数
 @contact: dinggan@whu.edu.cn
 """
 import sys, os
+
 if hasattr(sys, 'frozen'):
     os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
-from window import windowDS,window
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from window import windowDS
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSplashScreen
+from PyQt5.QtGui import QPixmap
 
 if __name__ == '__main__':
-    # 窗体构建（时间紧迫，未完成逻辑调用）
+    # 窗体构建
     app = QApplication(sys.argv)
+    # 创建启动界面，支持png透明图片
+    splash = QSplashScreen(QPixmap("./source/EMACS.png"))
+    splash.show()
+    # 可以显示启动信息
+    splash.showMessage('正在加载……')
+    # 载入配置文件
+    splash.showMessage('正在加载配置文件……')
+
     MainWindow = QMainWindow()
-    #已定界面 windows
+    # 已定界面 windows
     # ui = window.Ui_mainWindow()
     # 界面重构存储区域
     ui = windowDS.Ui_mainWindow()
